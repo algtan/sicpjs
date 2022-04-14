d52487f9a1e6138a49887dc985f9c8c501ee354d
Allen Tan <algtan.dev@gmail.com>
2022-04-14 16:23:12 -0700

    Create file for exercise 1.06

---
+++ chapter1/1.06.js
@@ -0,0 +1,29 @@
+// Section 1.1.7
+// page 20
+
+// Alyssa P. Hacker doesn't like the syntax of conditional expressions, involving the
+// characters '?' and ':'. "Why can't I just declare an ordinary conditoinal function
+// whose application works just like conditional expressions?" she asks. Alyssa's friend
+// Eva Lu Ator claims this can indeed be done, and she declares a 'conditional' function
+// as follows:
+
+function conditional(predicate, then_clause, else_clause) {
+    return predicate ? then_clause : else_clause;
+}
+
+// Eva demonstrates the program for Alyssa:
+
+conditional(2 === 3, 0, 5); // 5
+
+conditional(1 === 1, 0, 5); // 0
+
+// Delighted, Alyssa uses conditional to rewrite the square-root program:
+
+function sqrt_iter(guess, x) {
+    return conditional(is_good_enough(guess, x),
+                       guess,
+                       sqrt_iter(improve(guess, x),
+                                 x));
+}
+
+// What happens when Alyssa attempts to use this to compute square roots? Explain.
