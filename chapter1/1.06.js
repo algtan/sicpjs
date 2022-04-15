99b2039d246bda691f76b8b1cb7bc6e8ec10f853
Allen Tan <algtan.dev@gmail.com>
2022-04-15 14:48:44 -0700

    Add solution to exercise 1.06

--- chapter1/1.06.js
+++ chapter1/1.06.js
@@ -26,4 +26,28 @@ function sqrt_iter(guess, x) {
                                  x));
 }
 
+function improve(guess, x) {
+    return average(guess, x / guess);
+}
+
+function average(x, y) {
+    return (x + y) / 2;
+}
+
+function is_good_enough(guess, x) {
+    return math_abs(square(guess) - x) < 0.001;
+}
+
+function square(x) {
+    return math_pow(x, 2);
+}
+
+// Example function call
+sqrt_iter(1, 2);
+
 // What happens when Alyssa attempts to use this to compute square roots? Explain.
+
+// JavaScript uses applicative-order evaluation, which means that it will "evaluate
+// the arguments and then apply" the arguments to the function call. So every call
+// to 'sqrt_iter' will open a new call to 'sqrt_iter', which will lead to an
+// infinite loop/stack overflow as 'conditional' is never performed.
