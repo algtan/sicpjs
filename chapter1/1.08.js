0322226cf20df1bb68b62f6261e70a4fb46c7e06
Allen Tan <algtan.dev@gmail.com>
2022-04-16 09:23:54 -0700

    Create file for exercise 1.08

---
+++ chapter1/1.08.js
@@ -0,0 +1,11 @@
+// Section 1.1.7
+// page 21
+
+// Newton's method for cube roots is based on the fact that if 'y' is an approximation to the
+// cube root of 'x', then a better approximation is given by the value
+
+// ((x/y^2) + 2*y) / 3
+
+// Use this formula to implement a cube-root function analogous to the square-root function.
+// (In section 1.3.4 we will see how to implement Newton's method in general as an
+// abstraction of these square-root and cube-root functions.)
