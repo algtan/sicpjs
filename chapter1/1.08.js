a5ea68fd251e3889bb0b524d822af92cab7609a7
Allen Tan <algtan.dev@gmail.com>
2022-04-16 09:37:44 -0700

    Add solution to exercise 1.08

--- chapter1/1.08.js
+++ chapter1/1.08.js
@@ -9,3 +9,29 @@
 // Use this formula to implement a cube-root function analogous to the square-root function.
 // (In section 1.3.4 we will see how to implement Newton's method in general as an
 // abstraction of these square-root and cube-root functions.)
+
+function cbrt_iter(guess, x) {
+    return is_next_guess_good_enough(guess, x)  // check if following guess is good enough
+           ? improve(guess, x)                  // value of the next guess
+           : cbrt_iter(improve(guess, x), x);   // recursively call cbrt_iter to start following guess
+}
+
+function improve(guess, x) {
+    // guess = y in formula
+    return ((x / math_pow(guess, 2)) + 2 * guess) / 3;
+}
+
+function is_next_guess_good_enough(guess, x) {
+    return relative_error(guess, improve(guess, x)) < 0.001;
+}
+
+function relative_error(guess, nextGuess) {
+    return math_abs(guess - nextGuess) / nextGuess;
+}
+
+function cbrt(x) {
+    return cbrt_iter(1, x);
+}
+
+cbrt(27);
+cbrt(28);
