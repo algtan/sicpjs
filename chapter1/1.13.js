982c84f92c74da61e362fc066e792a31b9b8f7ab
Allen Tan <algtan.dev@gmail.com>
2022-04-20 15:14:28 -0700

    Create file for exercise 1.13

---
+++ chapter1/1.13.js
@@ -0,0 +1,6 @@
+// Section 1.2.2
+// page 36
+
+// Prove that Fib(n) is the closest integer to phi^n / sqrt(5), where phi = (1 + sqrt(5)) / 2. Hint: Use induction
+// and the definition of the Fibonacci numbers to prove that Fib(n) = (phi^n - Psi^n) / sqrt(5), where
+// Psi = (1 - sqrt(5)) / 2.
