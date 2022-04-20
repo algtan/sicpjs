8630e13cd00ce89056abb895c9cd9d1f0bd4dfd6
Allen Tan <algtan.dev@gmail.com>
2022-04-20 15:05:15 -0700

    Add solution to exercise 1.12

--- chapter1/1.12.js
+++ chapter1/1.12.js
@@ -13,3 +13,14 @@
 // The numbers at the edge of the triangle are all 1, and each number inside the triangle is the
 // sum of the two numbers above it. Write a function that computes elements of Pascal's
 // triangle by means of a recursive process.
+
+
+function pascal_triangle(row, index) {
+    return index <= 0 || row <= 0 || index > row
+           ? 0
+           : index === 1 || index === row
+           ? 1
+           : pascal_triangle(row-1, index-1) + pascal_triangle(row-1, index);
+}
+
+pascal_triangle(5, 3);
