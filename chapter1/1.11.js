7799b8259ed3dff9d94bf8385c10760dba3274ce
Allen Tan <algtan.dev@gmail.com>
2022-04-20 14:32:54 -0700

    Add solution to exercise 1.11

--- chapter1/1.11.js
+++ chapter1/1.11.js
@@ -5,3 +5,41 @@
 // if 'n >= 3'. Write a JavaScript function thate computes 'f' by means of a recursive process. Write
 // a function that computers 'f' by means of an iterative process.
 
+// Recursive
+
+function recursive_f(n) {
+    return n < 3
+           ? n
+           : recursive_f(n-1) + 2*recursive_f(n-2) + 3*recursive_f(n-3);
+}
+
+recursive_f(3); // 4
+recursive_f(4); // 11
+
+
+// Iterative
+
+function iterative_f(n) {
+    return n < 3
+           ? n
+           : iterative_helper(2, 1, 0, n);
+}
+
+// At the beginning, the parameters represent
+// a = f(i-1)
+// b = f(i-2)
+// c = f(i-3)
+// where 2 < i <= n
+function iterative_helper(a, b, c, count) {
+    return count - 2 === 0
+           ? a
+           : iterative_helper(a + 2*b + 3*c, a, b, count - 1);
+           // When we call iterative_helper in line 35
+           // a = f(i)
+           // b = f(i-1)
+           // c = f(i-2)
+           // where 2 < i <= n
+}
+
+iterative_f(3);
+iterative_f(4);
