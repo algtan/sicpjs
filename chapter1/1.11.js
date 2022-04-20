2250b2d2067a710a7b6c46d3a2fda12e7e06aa49
Allen Tan <algtan.dev@gmail.com>
2022-04-20 13:32:11 -0700

    Create file for exercise 1.11

---
+++ chapter1/1.11.js
@@ -0,0 +1,7 @@
+// Section 1.2.2
+// page 36
+
+// A function 'f' is defined by the rules 'f(n) = n' if  'n < 3' and  'f(n) = f(n-1) + 2*f(n-2) + 3*f(n-3)'
+// if 'n >= 3'. Write a JavaScript function thate computes 'f' by means of a recursive process. Write
+// a function that computers 'f' by means of an iterative process.
+
