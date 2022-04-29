bccddda50075182c4aec181228deb5ae0947a60b
Allen Tan <algtan.dev@gmail.com>
2022-04-28 17:15:37 -0700

    Add solution to exercise 1.16

--- chapter1/1.16.js
+++ chapter1/1.16.js
@@ -10,3 +10,35 @@
 // of the process. In general, the technique of defining an 'invariant quantity' that
 // remains unchanged from state to state is a powerful way to think about the design of
 // iterative algorithms.)
+
+function fast_exp_recursive(b, n) {
+    return n === 0
+           ? 1
+           : is_even(n)
+           ? square(fast_exp_recursive(b, n / 2))
+           : b * fast_exp_recursive(b, n - 1);
+}
+
+function square(x) {
+    return x * x;
+}
+
+function is_even(n) {
+    return n % 2 === 0;
+}
+
+
+function fast_exp_iter(a, b, n) {
+    return n === 0
+           ? a
+           : is_even(n)
+           ? fast_exp_iter(a, b * b, n / 2)
+           : fast_exp_iter(a * b, b, n - 1);
+}
+
+function fast_exp(b, n) {
+    return fast_exp_iter(1, b, n);
+}
+
+fast_exp(2, 4);
+fast_exp(2, 3);
