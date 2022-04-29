c4927231606983047b6eced3fc5d39c7ae67e858
Allen Tan <algtan.dev@gmail.com>
2022-04-29 15:26:32 -0700

    Create file for exercise 1.18

---
+++ chapter1/1.18.js
@@ -0,0 +1,6 @@
+// Section 1.2.4
+// page 40
+
+// Using the results of exercises 1.16 and 1.17, devise a function that generates an iterative
+// process for multiplying two integers in terms of adding, doubling, and halving and uses a
+// logarithmic number of steps.
