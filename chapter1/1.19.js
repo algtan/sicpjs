9a35c111566d35577e3c026e027dd9c541da3ab4
Allen Tan <algtan.dev@gmail.com>
2022-05-16 11:01:43 -0700

    Add remaining text for exercise 1.19

--- chapter1/1.19.js
+++ chapter1/1.19.js
@@ -5,4 +5,36 @@
 // of steps. Recall the transformation of the state variables 'a' and 'b' in the 'fib_iter' process of
 // section 1.2.2: 'a <- a + b' and 'b <- a'. Call this transformation 'T', and observe that applying 'T'
 // over and over again 'n' times, starting with 1 and 0, produces the pair Fib(n+1) and Fib(n).
+// In other words, the Fibonacci numbers are produced by applying 'T^n', the nth power of the
+// transformation 'T', starting with the pair (1,0). Now consider 'T' to be the special case of 'p=0'
+// and 'q=1' in a family of transformations 'T_pq', where 'T_pq' transforms the pair (a,b) according
+// to 'a <- bq + aq + ap' and 'b <- bp + aq'. Show that if we apply such a transformation 'T_pq' twice,
+// the effect is the same as using a single transformation "T_p'q'" of the same form, and compute
+// "p'" and "q'" in terms of 'p' and 'q'. This gives us an explicit way to square these transofrmations,
+// and thus we can compute 'T^n' using successive squaring, as in the 'fast_expt' function. Put
+// this all together to complete the following function, which runs in a logarithmic number of
+// steps:
 
+function fib(n) {
+    return fib_iter(1, 0, 0, 1, n);
+}
+
+function fib_iter(a, b, p, q, count) {
+    return count === 0
+           ? b
+           : is_even(count)
+           ? fib_iter(a,
+                      b,
+                      p_prime, // compute p'
+                      q_prime, // compute q'
+                      count / 2)
+           : fib_iter(b * q + a * q + a * p,
+                      b * p + a * q,
+                      p,
+                      q,
+                      count - 1);
+}
+
+function is_even(n) {
+    return n % 2 === 0;
+}
