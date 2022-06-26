37dc493bc24744e66459819942803818ff7fd74e
Allen Tan <algtan.dev@gmail.com>
2022-06-25 20:59:47 -0700

    Add solution to exercise 1.21

--- chapter1/1.21.js
+++ chapter1/1.21.js
@@ -3,3 +3,27 @@
 
 // Use the 'smallest_divisor' function to find the smallest divisor of each of the following
 // numbers: 199, 1999, 19999.
+
+function smallest_divisor(n) {
+    return find_divisor(n, 2);
+}
+
+function find_divisor(n, test_divisor) {
+    return square(test_divisor) > n
+        ? n
+        : divides(test_divisor, n)
+        ? test_divisor
+        : find_divisor(n, test_divisor + 1);
+}
+
+function divides(a, b) {
+    return b % a === 0;
+}
+
+function square(x) {
+    return x * x;
+}
+
+smallest_divisor(199); // 199
+smallest_divisor(1999); // 1999
+smallest_divisor(19999); // 7
