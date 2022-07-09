d010c0c6cc4e51bbe404248b0a8dc1968fb9e056
Allen Tan <algtan.dev@gmail.com>
2022-07-09 13:54:35 -0700

    Add solution to exercise 1.23

--- chapter1/1.23.js
+++ chapter1/1.23.js
@@ -12,3 +12,70 @@
 // this modification halves the number of test steps, you should expect it to run about twice
 // as fast. Is this expectation confirmed? If not, what is the observed ratio of the speeds of the
 // two algorithms, and how do you explain the fact that it is different from 2?
+
+function timed_prime_test(n) {
+    display(n);
+    return start_prime_test(n, get_time());
+}
+
+function start_prime_test(n, start_time) {
+    return is_prime(n)
+        ? report_prime(get_time() - start_time)
+        : true;
+}
+
+function report_prime(elapsed_time) {
+    display(" *** ");
+    display(elapsed_time);
+}
+
+function is_prime(n) {
+    return n === smallest_divisor(n);
+}
+
+function smallest_divisor(n) {
+    return find_divisor(n, 2);
+}
+
+function find_divisor(n, test_divisor) {
+    return square(test_divisor) > n
+        ? n
+        : divides(test_divisor, n)
+        ? test_divisor
+        : find_divisor(n, next(test_divisor));
+}
+
+function divides(a, b) {
+    return b % a === 0;
+}
+
+function square(x) {
+    return x * x;
+}
+
+
+function next(divisor) {
+    return divisor === 2
+        ? 3
+        : divisor + 2;
+}
+
+
+function search_for_primes(start_num, num_of_primes) {
+    return num_of_primes <= 0
+        ? true
+        : start_num <= 0 || (start_num % 2 === 0 && start_num > 2) // Numbers that are less than or equal to zero are not prime. Same with even numbers greater than 2.
+        ? search_for_primes(start_num + 1, num_of_primes)
+        : (start_num >= 1 && start_num <= 2) && is_undefined(timed_prime_test(start_num))
+        ? search_for_primes(start_num + 1, num_of_primes - 1)
+        : is_undefined(timed_prime_test(start_num))
+        ? search_for_primes(start_num + 2, num_of_primes - 1)
+        : search_for_primes(start_num + 2, num_of_primes);
+}
+
+search_for_primes(1000000, 3);
+search_for_primes(10000000, 3);
+
+// The expectation that the modification should run about twice as fast was not
+// confirmed, as it was about 1.5x as fast. This may be due to hardware or
+// network issues.
