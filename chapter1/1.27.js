30fe73918ef721be4316ee4b29fc26d04415e385
Allen Tan <algtan.dev@gmail.com>
2022-07-10 14:06:51 -0700

    Create file for exercise 1.27

---
+++ chapter1/1.27.js
@@ -0,0 +1,6 @@
+// Section 1.2.6
+// page 48
+
+// Demonstrate that the Carmichael numbers listed in footnote 45 really do fool the Fermat
+// test. That is, write a function that takes an integer 'n' and test whether 'a^n' is congruent to 'a
+// modulo n' for every 'a < n', and try your function on the given Carmichael numbers.
