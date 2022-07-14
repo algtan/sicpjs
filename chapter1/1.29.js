369886fa8c12c71b4e4eee5edd8c3401ed188dab
Allen Tan <algtan.dev@gmail.com>
2022-07-13 18:19:06 -0700

    Add solution to exercise 1.29

--- chapter1/1.29.js
+++ chapter1/1.29.js
@@ -12,3 +12,42 @@
 // and returns the value of the integral, computed using Simpson's Rule. Use your function
 // to integrate cube betweeen 0 and 1 (with n=100 and n=1000), and compare the results to
 // those of the 'integral' function shown above.
+
+function sum(term, a, next, b) {
+    return a > b
+        ? 0
+        : term(a) + sum(term, next(a), next, b);
+}
+
+function inc(n) {
+    return n + 1;
+}
+
+function simpson_rule(f, a, b, n) {
+    const h = (b - a) / n;
+    
+    function y(k) {
+        return f(a + k * h);
+    }
+    
+    function multiplier(k) {
+        return k === 0 || k === n
+            ? 1
+            : k % 2 === 0
+            ? 2
+            : 4;
+    }
+    
+    function simpson_rule_term(k) {
+        return multiplier(k) * y(k);
+    }
+    
+    return h / 3 * sum(simpson_rule_term, a, inc, n);
+}
+
+function cube(x) {
+    return x * x * x;
+}
+
+simpson_rule(cube, 0, 1, 100);
+simpson_rule(cube, 0, 1, 1000);
