ad6d4e734d6e71a9db232989b4b0d79fba33d1e8
Allen Tan <algtan.dev@gmail.com>
2022-07-18 16:27:21 -0700

    Add solution to exercise 1.30

--- chapter1/1.30.js
+++ chapter1/1.30.js
@@ -13,3 +13,12 @@
 //      }
 //      return iter(<??>, <??>);
 //  }
+
+function sum(term, a, next, b) {
+    function iter(a, result) {
+        return a > b
+            ? result
+            : iter(next(a), term(a) + result);
+    }
+    return iter(a, 0);
+}
