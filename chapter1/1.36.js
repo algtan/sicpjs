f51fbdec6a9fe51322829623c707b35413a41b59
Allen Tan <algtan.dev@gmail.com>
2022-08-28 11:24:53 -0700

    Add solution to exercise 1.36

--- chapter1/1.36.js
+++ chapter1/1.36.js
@@ -15,8 +15,8 @@ function fixed_point(f, first_guess) {
         return abs(x - y) < tolerance;
     }
     function try_with(guess) {
+        display(guess);  // Added 'display'
         const next = f(guess);
-        display(next);  // Added 'display'
         return close_enough(guess, next)
                ? next
                : try_with(next);
@@ -26,9 +26,15 @@ function fixed_point(f, first_guess) {
 
 // From page 14
 function abs(x) {
-    return x > 0
-        ? x
-        : x === 0
-        ? 0
-        : - x;
+    return x >= 0 ? x : - x;
 }
+
+// From page 19
+function average(x, y) {
+    return (x + y) / 2;
+}
+
+
+fixed_point(x => math_log(1000) / math_log(x), 2); // 1140 steps
+
+fixed_point(x => average(x, math_log(1000) / math_log(x)), 2); // 362 steps
