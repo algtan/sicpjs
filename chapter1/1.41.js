9cddd15cd8f718c100a7448da1b9c39da664817b
Allen Tan <algtan.dev@gmail.com>
2022-09-03 09:57:16 -0700

    Add additional cases for exercise 1.41

--- chapter1/1.41.js
+++ chapter1/1.41.js
@@ -24,3 +24,30 @@ double(double(double))(inc)(5); // 21
 // double(double)(inc)(5); // adds 2^2 (4)
 // double(double(double))(inc)(5) // adds 4^2 (16)
 // double(double(double(double)))(inc)(5); // adds 16^2 (256)
+
+function plus_two(x) {
+  return x + 2;
+}
+
+double(plus_two)(1); // adds 4 (2 * 2^1) for a final answer of 5
+double(double(plus_two))(1); // adds 8 (2 * 2^2) for a final answer of 9
+double(double(double))(plus_two)(1); // adds 32 (2 * 2^4) for a final answer of 33
+double(double(double(double)))(plus_two)(1); // adds 512 (2 * 2^8) for a final answer of 513
+
+function plus_three(x) {
+  return x + 3;
+}
+
+double(plus_three)(1); // adds 6 (3 * 2^1) for a final answer of 7
+double(double(plus_three))(1); // adds 12 (3 * 2^2) for a final answer of 9
+double(double(double))(plus_three)(1); // adds 48 (3 * 2^4) for a final answer of 49
+double(double(double(double)))(plus_three)(1); // adds 768 (3 * 2^8) for a final answer of 513
+
+function plus_five(x) {
+  return x + 5;
+}
+
+double(plus_five)(1); // adds 10 (5 * 2^1) for a final answer of 11
+double(double(plus_five))(1); // adds 20 (5 * 2^2) for a final answer of 21
+double(double(double))(plus_five)(1); // adds 80 (5 * 2^4) for a final answer of 81
+double(double(double(double)))(plus_five)(1); // adds 1280 (5 * 2^8) for a final answer of 1281
