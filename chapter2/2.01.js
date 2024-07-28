fd7b84bdd1044df6829d0993100c886c9c3a7582
Allen Tan <algtan.dev@gmail.com>
2024-07-28 15:21:07 -0700

    Create file for exercise 2.01

---
+++ chapter2/2.01.js
@@ -0,0 +1,17 @@
+// Section 2.1.1
+// page 76
+
+// Definte a better version of 'make_rat' that handles both positive and
+// negative arguments. The function 'make_rat' should normalize the sign so
+// that if the rational number is positive, both the numerator and denominator
+// are positive, and if the rational number is negative, only the numerator
+// is negative.
+
+// p. 74
+// function make_rat(n, d) {
+//    return pair(n, d);
+//}
+
+function make_rat(n, d) {
+    
+}
