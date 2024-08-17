503a17c999c760fa7699501b221fcb93e3787cba
Allen Tan <algtan.dev@gmail.com>
2024-08-17 16:28:00 -0700

    Add solution to exercise 2.04

--- chapter2/2.04.js
+++ chapter2/2.04.js
@@ -15,3 +15,24 @@ function head(z) {
 
 // What is the corresponding definition of tail? (Hint: To verify that
 // this works, make use of the substitution model of section 1.1.5.) 
+
+function tail(z) {
+    return z((p, q) => q);
+}
+
+// tail(pair(x, y));
+
+// substitute 'pair(x, y)' with 'm => m(x, y)'
+// tail(m => m(x, y))
+
+// substitute 'z' with 'm => m(x, y)'
+// (m => m(x, y))((p, q) => q)
+
+// substitute 'm' with '(p, q) => q'
+// ((p, q) => q)(x, y)
+
+// substitute 'p' with 'x', and 'q' with 'y'
+// (x, y) => y
+
+// result
+// y
