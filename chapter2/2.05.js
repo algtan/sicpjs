ccba4bf9d98e6fdb7dfaa60c6019e9f9e652a4d2
Allen Tan <algtan.dev@gmail.com>
2024-08-25 20:10:57 -0700

    Add solution to exercise 2.05

--- chapter2/2.05.js
+++ chapter2/2.05.js
@@ -6,3 +6,17 @@
 // that is the product '2^a * 3^b'. Give the corresponding definitions of the
 // functions 'pair', 'head', and 'tail'.
 
+function new_pair(a, b) {
+    return math_pow(2, a) * math_pow(3, b);
+}
+
+function new_head(p) {
+    return p % 2 === 0 ? new_head(p / 2) + 1 : 0;
+}
+
+function new_tail(p) {
+    return p % 3 === 0 ? new_tail(p / 3) + 1 : 0;
+}
+
+new_head(new_pair(2, 1)); // 2
+new_tail(new_pair(1, 3)); // 3
