50a7d55b6ebbf06a083f08defa558f9c5f509d10
Allen <No public email provided>
2024-08-25 19:49:07 -0700

    Create file for exercise 2.05

---
+++ chapter2/2.05.js
@@ -0,0 +1,8 @@
+// Section 2.1.3
+// page 80
+
+// Show that we can represent pairs of nonnegative integers using only numbers
+// and arithmetic operations if we represent the pair 'a' and 'b' as the integer
+// that is the product '2^a * 3^b'. Give the corresponding definitions of the
+// functions 'pair', 'head', and 'tail'.
+
