9eec92d0052b6860e1eefd206c92ce1a905763fd
Allen Tan <algtan.dev@gmail.com>
2025-06-08 15:35:53 -0700

    Create file for exercise 2.07

---
+++ chapter2/2.07.js
@@ -0,0 +1,15 @@
+// Section 2.1.4
+// page 82
+
+// Exercise 2.7
+
+// Alyssa's program is incomplete because she has not specified the
+// implementation of the interval abstraction. Here is a definition of the
+// interval constructor:
+
+function make_interval(x, y) {
+    return pair(x, y);
+}
+
+// Define selectors 'upper_bound' and 'lower_bound' to complete the
+// implementation.
