83cc8a0e8ff5deee2abb4f80801a90f45f07ea7d
Allen <No public email provided>
2025-06-08 16:46:58 -0700

    fixup! Create file for exercise 2.12

--- chapter2/2.12.js
+++ chapter2/2.12.js
@@ -4,7 +4,7 @@
 // Exercise 2.12
 
 // Define a constructor 'make_center_percent' that takes a center and a
-// percentage toelrance and produces the desired interval. You must also
+// percentage tolerance and produces the desired interval. You must also
 // define a selector 'percent' that produces the percentage tolerance for a
 // given interval. The 'center' selector is the same as the one shown above.
 
