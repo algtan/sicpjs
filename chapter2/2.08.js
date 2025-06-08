26d1121004714b65a5212aae6e1b5a8c5021dd75
Allen Tan <algtan.dev@gmail.com>
2025-06-08 15:57:31 -0700

    Add solution to exercise 2.08

--- chapter2/2.08.js
+++ chapter2/2.08.js
@@ -6,3 +6,28 @@
 // Using reasoning analogous to Alyssa's, describe how the difference of two
 // intervals may be computed. Define a corresponding subraction function, called
 // 'sub_interval'.
+
+// The difference between two intervals itself is an interval. To find the
+// smallest value of this interval, you need to take the smallest value of
+// interval 1, and subtract the largest value of interval 2 from it. To find the
+// largest value of this interval, you need to take the largest value of
+// interval 1, and subtract the smallest value of interval 2 from it.
+
+// from Exercise 2.7:
+function make_interval(x, y) {
+    return pair(x, y);
+}
+
+function upper_bound(i) {
+    return tail(i);
+}
+
+function lower_bound(i) {
+    return head(i);
+}
+
+
+function sub_interval(x, y) {
+    return make_interval(lower_bound(x) - upper_bound(y),
+                         upper_bound(x) - lower_bound(y));
+}
