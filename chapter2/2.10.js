e57d67d32af5ee2b99e36aac646b0d01cb1b5159
Allen <No public email provided>
2025-06-08 16:23:04 -0700

    Create file for exercise 2.10

---
+++ chapter2/2.10.js
@@ -0,0 +1,9 @@
+// Section 2.1.4
+// page 82
+
+// Exercise 2.10
+
+// Ben Bitdiddle, an expert systems programmer, looks over Alyssa's shoulder
+// and comments that it is not clear what it means to divide by an interval
+// that spans zero. Modify Alyssa's program to check for this condition and to
+// signal an error if it occurs.
