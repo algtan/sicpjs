7f0fbb861b52c2ed2e5bba785e5ebab67f795277
Allen Tan <algtan.dev@gmail.com>
2025-06-08 15:42:59 -0700

    Create file for exercise 2.08

---
+++ chapter2/2.08.js
@@ -0,0 +1,8 @@
+// Section 2.1.4
+// page 82
+
+// Exercise 2.8
+
+// Using reasoning analogous to Alyssa's, describe how the difference of two
+// intervals may be computed. Define a corresponding subraction function, called
+// 'sub_interval'.
