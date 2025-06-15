a29a2aaa60deecb08689c8dab90cd7da121520fd
Allen <No public email provided>
2025-06-14 20:55:45 -0700

    Add solution to exercise 2.22

--- chapter2/2.22.js
+++ chapter2/2.22.js
@@ -21,6 +21,8 @@
 // Unfortunately, defining 'square_list' this way procues the answer list in the
 // reverse order of the one desired. Why?
 
+// The list is in reverse order because as we traverse the list, the answer for
+// the current element is being prepended to the list of answers.
 
 // Part b
 // Louis then tries to fix his bug by interchanging the arguments to pair:
@@ -33,6 +35,10 @@
 //                     pair(answer,
 //                          square(head(things))));
 //     }
+//     return iter(items, null);
 // }
 
-// This doesnt work either. Explain.
+// This doesn't work either. Explain.
+
+// This doesn't work either because we start creating a list of answers with
+// 'null' at the very front of it.
