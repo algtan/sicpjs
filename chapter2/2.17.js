ede21bd1764a1ae264d571708aaeb8cf42cfc5a1
Allen Tan <algtan.dev@gmail.com>
2025-06-14 19:58:24 -0700

    Create file for exercise 2.17

---
+++ chapter2/2.17.js
@@ -0,0 +1,10 @@
+// Section 2.2.1
+// page 89
+
+// Exercise 2.17
+
+// Define a function 'last_pair' that returns the list that contains only the
+// last element of a given (nonempty) list:
+
+// last_pair(list(23, 72, 149, 34))
+// list(34)
