f84e14e379f50030b28c41f4bd1254eb4e144502
Allen Tan <algtan.dev@gmail.com>
2025-08-08 17:33:24 -0700

    Create file for exercise 2.26

---
+++ chapter2/2.26.js
@@ -0,0 +1,18 @@
+// Section 2.2.2
+// page 95
+
+// Exercise 2.26
+
+// Suppose we define 'x' and 'y' to be two lists:
+
+const x = list(1, 2, 3);
+const y = list(4, 5, 6);
+
+// What is the result of evaluating each of the following expressions, in box
+// notation and list notation?
+
+// append(x, y)
+
+// pair(x, y)
+
+// list(x, y)
